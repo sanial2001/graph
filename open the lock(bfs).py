b17d30add857290a51e7f563dc6456fb7c2e180d
sanial2001 <sanial2001@gmail.com>
2022-05-13 19:10:58 +0530

    new commit

--- open the lock(bfs).py
+++ open the lock(bfs).py
@@ -1,4 +1,45 @@
 class Solution:
+    def __init__(self):
+        self.d = {
+            '0': ['9', '1'],
+            '1': ['0', '2'],
+            '2': ['1', '3'],
+            '3': ['2', '4'],
+            '4': ['3', '5'],
+            '5': ['4', '6'],
+            '6': ['5', '7'],
+            '7': ['6', '8'],
+            '8': ['7', '9'],
+            '9': ['8', '0'],
+        }
+
+    def openLock(self, deadends: List[str], target: str) -> int:
+        visit = set()
+        visit.add('0000')
+        q = ['0000']
+        steps = 0
+
+        while q:
+            num = len(q)
+            for i in range(num):
+                match = q.pop(0)
+                if match == target:
+                    return steps
+                if match in deadends:
+                    continue
+                for i in range(4):
+                    key = match[i]
+                    for j in range(2):
+                        temp = match[:i] + self.d[key][j] + match[i + 1:]
+                        # print(match, temp)
+                        if temp not in visit:
+                            visit.add(temp)
+                            q.append(temp)
+            if q: steps += 1
+        return -1
+
+
+class Solution:
     def children(self, nums):
         nums = list(nums)
         ans = []
